5ae3184f79b443ddf73db21a1cac01dc05c3902b
wipegup <wipegup@gmail.com>
2019-01-23 12:52:18 -0700

    Add start to deck_test

---
+++ test/deck_test.rb
@@ -0,0 +1,46 @@
+require 'minitest/autorun'
+require 'minitest/pride'
+require './lib/Card'
+require './lib/Deck'
+
+class DeckTest < Minitest::Test
+  questions = ["2+2", "5+3","4+10", "7+14"]
+  answers = [4,8,14,21]
+  categories = [:Geography, :STEM, :STEM, :Math]
+  cards = []
+
+  questions.zip(answers, categories).each do |args|
+    cards << Card.new(*args)
+  end
+
+
+  def test_it_exists
+
+    deck = Deck.new(cards)
+
+    assert_instance_of Deck, deck
+
+  end
+
+  def test_it_has_cards
+    # assert # Do card test on each card?
+  end
+
+  def test_it_has_count
+
+    deck = Deck.new(cards)
+
+    assert_equal deck.count, questions.length
+  end
+
+  def test_it_has_cards_in_category
+
+    deck = Deck.new(cards)
+
+    categories.uniq.each do |cat|
+
+      assert_equal deck.cards_in_category(cat) categories.count(cat)
+    end
+
+    
+  end
