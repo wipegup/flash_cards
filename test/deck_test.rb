db7b61c8ca538c8d3634aa1a5bb9203d9d78c0e9
wipegup <wipegup@gmail.com>
2019-01-24 15:51:51 -0700

    Finish deck_test / fixed deck from test

--- test/deck_test.rb
+++ test/deck_test.rb
@@ -4,48 +4,42 @@ require './lib/Card'
 require './lib/Deck'
 
 class DeckTest < Minitest::Test
-  questions = ["2+2", "5+3","4+10", "7+14"]
-  answers = [4,8,14,21]
-  categories = [:Geography, :STEM, :STEM, :Math]
-  cards = []
+  def setup
+    # Create list of Questions,Answers, and Categories
+    @questions = ["2+2", "5+3","4+10", "7+14"]
+    @answers = [4,8,14,21]
+    @categories = [:Geography, :STEM, :STEM, :Math]
 
-  questions.zip(answers, categories).each do |args|
-    cards << Card.new(*args)
+    # Create Cards and add to list
+    @cards = []
+
+    @questions.zip(@answers, @categories).each do |args|
+      @cards << Card.new(*args)
+    end
+
+    # Build Deck
+    @deck = Deck.new(@cards)
   end
 
-
   def test_it_exists
-
-    deck = Deck.new(cards)
-
-    assert_instance_of Deck, deck
-
+    assert_instance_of Deck, @deck
   end
 
   def test_it_has_cards
-    deck = Deck.new(cards)
-
-    deck.cards.each do |card|
+    @deck.cards.each do |card|
       assert_instance_of Card, card
     end
 
   end
 
   def test_it_has_count
-
-    deck = Deck.new(cards)
-
-    assert_equal deck.count, questions.length
+    assert_equal @deck.count, @questions.length
   end
 
   def test_it_has_cards_in_category
-
-    deck = Deck.new(cards)
-
-    categories.uniq.each do |cat|
-
-      assert_equal deck.cards_in_category(cat) categories.count(cat)
+    @categories.uniq.each do |cat|
+      assert_equal @deck.cards_in_category(cat), @categories.count(cat)
     end
 
-
   end
+end
