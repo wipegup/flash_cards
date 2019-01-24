b92fb2230741011006a31c951017a3adf2439687
wipegup <wipegup@gmail.com>
2019-01-24 14:44:44 -0700

    Finish turn_test setup

--- test/turn_test.rb
+++ test/turn_test.rb
@@ -10,47 +10,35 @@ class TurnTest < Minitest::Test
     @guess_true = "Juneau"
     @guess_false = "Washington"
 
+    @turn_t = Turn.new(@guess_true, @card)
+    @turn_f = Turn.new(@guess_false, @card)
+
   end
 
 
   def test_it_exists
-    turn_t = Turn.new(@guess_true, @card)
-    turn_f = Turn.new(@guess_false, @card)
 
-    assert_instance_of Turn, turn_t
-    assert_instance_of Turn, turn_f
+    assert_instance_of Turn, @turn_t
+    assert_instance_of Turn, @turn_f
   end
 
   def test_it_has_guess
-    turn_t = Turn.new(@guess_true, @card)
-    turn_f = Turn.new(@guess_false, @card)
-
-    assert_equal @guess_true, turn_t.guess
-    assert_equal @guess_false, turn_f.guess
+    assert_equal @guess_true, @turn_t.guess
+    assert_equal @guess_false, @turn_f.guess
   end
 
   def test_it_has_card
-    turn_t = Turn.new(@guess_true, @card)
-    turn_f = Turn.new(@guess_false, @card)
-
-    assert_instance_of Card, turn_t.card
-    assert_instance_of Card, turn_f.card
+    assert_instance_of Card, @turn_t.card
+    assert_instance_of Card, @turn_f.card
   end
 
   def test_it_has_correct?
-    turn_t = Turn.new(@guess_true, @card)
-    turn_f = Turn.new(@guess_false, @card)
-
-    assert_equal true, turn_t.correct?
-    assert_equal false, turn_f.correct?
-
+    assert_equal true, @turn_t.correct?
+    assert_equal false, @turn_f.correct?
   end
 
   def test_it_has_feedback
-    turn_t = Turn.new(@guess_true, @card)
-    turn_f = Turn.new(@guess_false, @card)
-
-    assert_equal "Correct!", turn_t.feedback
-    assert_equal "Incorrect.", turn_f.feedback
+    assert_equal "Correct!", @turn_t.feedback
+    assert_equal "Incorrect.", @turn_f.feedback
   end
 end
