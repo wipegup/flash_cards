038453a593a45ae4975ab7d732a0deeadb40dca3
wipegup <wipegup@gmail.com>
2019-01-24 13:30:40 -0700

    Change vars to instance vars

--- test/turn_test.rb
+++ test/turn_test.rb
@@ -5,37 +5,37 @@ require './lib/turn'
 
 class TurnTest < Minitest::Test
 
-  card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
-  guess_true = "Juneau"
-  guess_false = "Washington"
+  @card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+  @guess_true = "Juneau"
+  @guess_false = "Washington"
 
   def test_it_exists
-    turn_t = Turn.new(guess_true, card)
-    turn_f = Turn.new(guess_false, card)
+    turn_t = Turn.new(@guess_true, @card)
+    turn_f = Turn.new(@guess_false, @card)
 
     assert_instance_of Turn, turn_t
     assert_instance_of Turn, turn_f
   end
 
   def test_it_has_guess
-    turn_t = Turn.new(guess_true, card)
-    turn_f = Turn.new(guess_false, card)
+    turn_t = Turn.new(@guess_true, @card)
+    turn_f = Turn.new(@guess_false, @card)
 
-    assert_equal guess_true, turn_t.guess
-    assert_equal guess_false, turn_f.guess
+    assert_equal @guess_true, turn_t.guess
+    assert_equal @guess_false, turn_f.guess
   end
 
   def test_it_has_card
-    turn_t = Turn.new(guess_true, card)
-    turn_f = Turn.new(guess_false, card)
+    turn_t = Turn.new(@guess_true, @card)
+    turn_f = Turn.new(@guess_false, @card)
 
     assert_instance_of Card, turn_t.card
     assert_instance_of Card, turn_f.card
   end
 
   def test_it_has_correct?
-    turn_t = Turn.new(guess_true, card)
-    turn_f = Turn.new(guess_false, card)
+    turn_t = Turn.new(@guess_true, @card)
+    turn_f = Turn.new(@guess_false, @card)
 
     assert_equal true, turn_t.correct?
     assert_equal false, turn_f.correct?
@@ -43,8 +43,8 @@ class TurnTest < Minitest::Test
   end
 
   def test_it_has_feedback
-    turn_t = Turn.new(guess_true, card)
-    turn_f = Turn.new(guess_false, card)
+    turn_t = Turn.new(@guess_true, @card)
+    turn_f = Turn.new(@guess_false, @card)
 
     assert_equal "Correct!", turn_t.feedback
     assert_equal "Incorrect.", turn_f.feedback
