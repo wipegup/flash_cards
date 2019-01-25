25532b4cb0afb9b51afcc82700fc6affeb494eb2
wipegup <wipegup@gmail.com>
2019-01-25 11:22:48 -0700

    Finish up round and round_test

--- lib/Round.rb
+++ lib/Round.rb
@@ -4,29 +4,42 @@ class Round
   def initialize(deck)
     @deck = deck
     @turns = []
-    @number_correct = 0 # change to method
+    # @number_correct = 0 # change to method
   end
 
   def current_card
-    return @deck[@turns.length]
+    return @deck.cards[@turns.length]
   end
 
   def take_turn(guess)
     turn =  Turn.new(guess, current_card)
     @turns << turn
 
-    if turn.correct?
-      @number_correct +=1
-    end
+    # if turn.correct?
+    #   @number_correct +=1
+    # end
 
     return turn
 
   end
 
-  def number_correct_by_category(category)
-
+  def number_correct
+    num_correct = 0
     @turns.each do |turn|
-        ####
+      if turn.correct?
+        num_correct +=1
+      end
+    end
+
+    return num_correct
+  end
+
+  def number_correct_by_category(category)
+    correct_by_category = 0
+    @turns.each do |turn|
+        if turn.correct? && turn.card.category == category
+          correct_by_category +=1
+        end
     end
   end
 
--- test/round_test.rb
+++ test/round_test.rb
@@ -1,12 +1,13 @@
 require 'minitest/autorun'
 require './lib/card'
 require './lib/round'
+require './lib/deck'
 require './lib/turn'
 
 class RoundTest < Minitest::Test
 
   def setup
-    # Create list of Questions,Answers, and Categories
+    # Create Question, Answer and Category for cards
     card_info = [
       ["2+2", 4, :Geography],
       ["5+3", 8, :STEM],
@@ -30,12 +31,51 @@ class RoundTest < Minitest::Test
   end
 
   def test_it_has_attributes
-    assert_equal @deck, round.deck
+    assert_equal @deck, @round.deck
     # assert_equal 0, round.number_correct
-    assert_equal [], round.turns
+    assert_equal [], @round.turns
   end
 
   def test_it_has_current_card
-    # assert_
+    num_previous_turns = @round.turns.length
+    assert_equal @cards[num_previous_turns], @round.current_card
+  end
+
+  def test_it_has_take_turn
+    assert_instance_of Turn, @round.take_turn(4)
+  end
+
+  def test_take_turn_adds_to_turn_array
+    num_previous_turns = @round.turns.length
+    turn = @round.take_turn(4)
+    assert_equal num_previous_turns+1, @round.turns.length
+  end
+
+  def test_take_turn_returns_and_stores_correct_turn
+    num_previous_turns = @round.turns.length
+    # Correct answer for next card
+    guess = @cards[num_previous_turns].answer
+
+    # Guess correctly
+    return_from_correct_turn = @round.take_turn(guess)
+    assert_equal return_from_correct_turn, @round.turns[-1]
+
+    # Guess incorrectly on subsequent card
+    return_from_incorrect_turn = @round.take_turn(guess)
+    assert_equal return_from_incorrect_turn, @round.turns[-1]
+  end
+
+  def test_number_correct
+    turns = @round.turns
+    num_correct = 0
+
+    turns.each do |turn|
+      if turn.correct?
+        num_correct +=1
+      end
+    end
+
+    assert_equal num_correct, @round.number_correct
+
   end
 end
