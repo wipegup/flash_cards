49e238472e1c5b6b886401ca6d50b17013bb1869
wipegup <wipegup@gmail.com>
2019-01-27 08:11:00 -0700

    Changed to using card_gen; Changed to calling more methods in round

--- flashcard_runner.rb
+++ flashcard_runner.rb
@@ -1,16 +1,21 @@
 require './lib/round'
+require './lib/card_generator'
 
-card_info = [
-  ["2+2", 4, :Geography],
-  ["5+3", 8, :STEM],
-  ["4+10", 14, :STEM],
-  ["7+14", 21, :Math]
-]
+cards = CardGenerator.new('./lib/cards.txt').cards
 
-cards = []
-card_info.each do |card_arg|
-  cards << Card.new(*card_arg)
-end
+p cards.length
+
+# card_info = [
+#   ["2+2", 4, :Geography],
+#   ["5+3", 8, :STEM],
+#   ["4+10", 14, :STEM],
+#   ["7+14", 21, :Math]
+# ]
+#
+# cards = []
+# card_info.each do |card_arg|
+#   cards << Card.new(*card_arg)
+# end
 
 # Build Deck
 deck = Deck.new(cards)
@@ -18,3 +23,5 @@ deck = Deck.new(cards)
 round = Round.new(deck)
 
 round.start
+round.play_game
+round.round_summary
